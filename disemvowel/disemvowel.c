ef174e0e6f6bc47a9562878aa7851a7080e46059
Jadyn Sondrol <sondr037@morris.umn.edu>
2021-09-28 11:45:46 -0500

    Made disemvowel method

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,32 @@
 #include <stdlib.h>
-
+#include <string.h>
+#include <stdbool.h>
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+bool isVowel(char letter){
+	return (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' ||letter == 'u' || letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U');
 }
+
+char *disemvowel(char *str) {
+  int inlen = strlen(str);
+  int outlen=0;
+  for(int i=0; i<inlen; i++){
+   if(!isVowel(str[i])){
+	outlen++;	   
+   }
+  }
+  char* output = (char*) calloc(outlen + 1, sizeof(char));
+  int outpos = 0;
+  for(int i=0; i<inlen; i++){
+    if(!isVowel(str[i])){
+      output[outpos]=str[i];
+      if(++outpos >= outlen){
+	      break;
+      }
+    }
+  }
+  output[outpos] = '\0';
+  
+  return output;
+}
+
